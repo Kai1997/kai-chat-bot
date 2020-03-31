0dca75218eeb4fa7ff7ad118e3a49d2a6f74ffe1
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-03-31 20:59:43 +0700

    v5 5

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post('/webhook', async function(req, res) { // Phần sử lý tin nhắn c�
       var senderId = message.sender.id;
       if (message.message) {
         var text = message.message.text;
+        sendMessage(senderId, text);
         const request = {
 			session: sessionPath,
 			queryInput: {
