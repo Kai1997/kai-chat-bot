2fa064e606272e4434774bc78772c8a4b39a8fd4
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-05-05 19:36:48 +0700

    cronjob 23

--- index.js
+++ index.js
@@ -22,10 +22,15 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
-cron.schedule("20 11 * * *", function () {
+cron.schedule("20 15 * * *", function () {
   sendMessage("2307959022584072", " LỊCH NGÀY MAI " + getMyDate() + " " + getTomorrow());
 
   resFromDialog("2307959022584072", getMyDate())
+});
+cron.schedule("20 15 * * *", function () {
+  sendMessage("2892699947443175", " LỊCH NGÀY MAI " + getMyDate() + " " + getTomorrow());
+
+  resFromDialog("2892699947443175", getMyDate())
 });
 var request = require("request");
 
