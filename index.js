b2c0e33a07e961de52fb0d6bfb956c60e01cd08a
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-05-05 23:11:55 +0700

    cronjob 28

--- index.js
+++ index.js
@@ -140,18 +140,18 @@ function sendQuickReply(senderId, message) {
       recipient: {
         id: senderId
       },
-      "message":{
-        "text":"Pick a color:",
-        "quick_replies":[
+      message:{
+        text:"Pick a color:",
+        quick_replies:[
           {
-            "content_type":"text",
-            "title":"Red",
-            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+            content_type:"text",
+            title:"Red",
+            payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
           },
           {
-            "content_type":"text",
-            "title":"Green",
-            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+            content_type:"text",
+            title:"Green",
+            payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
           }
         ]
       }
