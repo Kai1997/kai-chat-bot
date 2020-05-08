4c70c873bb6223be07d5a951f35721a0e06f0332
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-05-08 16:57:25 +0700

    new

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
-cron.schedule("59 12 * * *", function () {
+cron.schedule("59 11 * * *", function () {
   sendMessage("2307959022584072", " LỊCH NGÀY MAI " + getMyDate() + " " + getTomorrow());
 
   resFromDialog("2307959022584072", getMyDate())
 });
-cron.schedule("59 12 * * *", function () {
+cron.schedule("59 11 * * *", function () {
   sendMessage("2892699947443175", " LỊCH NGÀY MAI " + getMyDate() + " " + getTomorrow());
 
   resFromDialog("2892699947443175", getMyDate())
